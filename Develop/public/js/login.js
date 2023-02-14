9e4206f5b1c0b82ec5223c55bc14a10f47760bc2
Grey P <greytpearson@gmail.com>
2023-02-13 17:21:27 -0700

    added basic log in functions

--- Develop/public/js/login.js
+++ Develop/public/js/login.js
@@ -7,7 +7,44 @@ const signIn = async (event) => {
     // verify all fields are full
     if (email && password) {
         // POST response
+        const respond = await fetch('/api/user/login', {
+            method: 'POST',
+            body: JSON.stringify({ email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        })
         // if ok doc.location.replace(somewhere)
+        if (respond.ok) {
+            document.location.replace('/pofile')
+        } else {
+            alert(respond.statusText)
+        }
+        // else error
+    } else {
+        window.alert('your missing email or password')
+    }
+}
+
+const signUp = async (event) => {
+    // prevent default
+    event.preventDefault()
+    // grab fields
+    const username = document.querySelector('.hero-layout-4-username').ariaValueMax.trim()
+    const email = document.querySelector('.hero-layout-4-email').ariaValueMax.trim()
+    const password = document.querySelector('.hero-layout-4-Password').ariaValueMax.trim()
+    // verify all fields are full
+    if (username && email && password) {
+        // POST response
+        const respond = await fetch('/api/user/login', { // change to something else?
+            method: 'POST',
+            body: JSON.stringify({ username, email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        })
+        // if ok doc.location.replace(somewhere)
+        if (respond.ok) {
+            document.location.replace('/pofile') // where do we want to send the user
+        } else {
+            alert(respond.statusText)
+        }
         // else error
     } else {
         window.alert('your missing email or password')
