65821ecc2cf0effbb81584b544b44ba2f73070a5
Hans Shen <hshen801@gmail.com>
2023-02-15 12:17:21 -0700

    Added more search functionality from last.fm API to generate more data

--- Develop/public/js/homepage.js
+++ Develop/public/js/homepage.js
@@ -21,3 +21,19 @@ const artistButtons = document.getElementsByClassName("artist-button");
 for (const button of artistButtons) {
     button.addEventListener("click", showArtist);
 }
+
+function showTrack(event) {
+    const track = event.target.innerText;
+    const artist = event.target.getAttribute("data-artist");
+    const endIndex = window.location.href.indexOf("&");
+    if (endIndex == -1)
+    window.location.href += (`&artist=${artist}&track=${track}`);
+    else 
+    window.location.href = window.location.href.slice(0, endIndex) + (`&artist=${artist}&track=${track}`);
+}
+
+const trackButtons = document.getElementsByClassName("track-button");
+for (const button of trackButtons) {
+    button.addEventListener("click", showTrack);
+}
+
