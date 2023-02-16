9a6873f6123a83554123f09b0f66a224ba2a95e2
Grey P <greytpearson@gmail.com>
2023-02-15 20:52:48 -0700

    user can log in and sign up

--- Develop/public/js/login.js
+++ Develop/public/js/login.js
@@ -1,20 +1,16 @@
-console.log(3)
+let form = document.querySelector('#userform')
+
 const signInCall = async (event) => {
     // prevent default
     event.preventDefault()
 
-    const formData = new FormData(this.signin)
-    for (const pair of formData.entries()) {
-        console.log(pair)
-    }
     // grab fields
     const email = document.getElementById('email').value
-    console.log(email)
     const password = document.getElementById('password').value
+
     // verify all fields are full
     if (email && password) {
         // POST response
-        console.log('testing bitches')
         const respond = await fetch('/api/user/login', {
             method: 'POST',
             body: JSON.stringify({ email, password }),
@@ -27,37 +23,36 @@ const signInCall = async (event) => {
             alert(respond.statusText)
         }
         // else error
-        console.log('we made it')
     } else {
         window.alert('your missing email or password')
     }
 }
 
-// const signUp = async (event) => {
-//     // prevent default
-//     event.preventDefault()
-//     // grab fields
-//     const email = document.querySelector('.hero-layout-4-email').ariaValueMax.trim()
-//     const password = document.querySelector('.hero-layout-4-Password').ariaValueMax.trim()
-//     // verify all fields are full
-//     if (email && password) {
-//         // POST response
-//         const respond = await fetch('/api/user', { // change to something else?
-//             method: 'POST',
-//             body: JSON.stringify({ email, password }),
-//             headers: { 'Content-Type': 'application/json' },
-//         })
-//         // if ok doc.location.replace(somewhere)
-//         if (respond.ok) {
-//             document.location.replace('/pofile') // where do we want to send the user
-//         } else {
-//             alert(respond.statusText)
-//         }
-//         // else error
-//     } else {
-//         window.alert('your missing email or password')
-//     }
-// }
+const register = async (event) => {
+    // prevent default
+    event.preventDefault()
+    // grab fields
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+    // verify all fields are full
+    if (email && password) {
+        // POST response
+        const respond = await fetch('/api/user', { // change to something else?
+            method: 'POST',
+            body: JSON.stringify({ email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        })
+        // if ok doc.location.replace(somewhere)
+        if (respond.ok) {
+            document.location.replace('/profile') // where do we want to send the user
+        } else {
+            alert(respond.statusText)
+        }
+        // else error
+    } else {
+        window.alert('your missing email or password')
+    }
+}
 
 // document
 //     .querySelector('.signin')
@@ -65,5 +60,10 @@ const signInCall = async (event) => {
 
 // document.querySelector('.signup').addEventListener('submit', signUp)
 
-let form = document.getElementById('signin')
-form.addEventListener('submit', signInCall)
+
+// form.addEventListener('submit', signInCall)
+
+// document
+//     .querySelector('#userform')
+//     .addEventListener('submit', signInCall);
+
