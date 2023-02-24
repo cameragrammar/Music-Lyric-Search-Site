5019111ab058998a623f026036a278a34ae2ebac
Hans Shen <116024825+cameragrammar@users.noreply.github.com>
2023-02-23 17:25:21 -0700

    comment bcrypt back in

--- Develop/models/user.js
+++ Develop/models/user.js
@@ -1,11 +1,10 @@
 const { Model, DataTypes } = require('sequelize');
-//const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
 class User extends Model {
     checkPassword(loginPw) {
-        //return bcrypt.compareSync(loginPw, this.password);
-        return true;
+        return bcrypt.compareSync(loginPw, this.password);
     }
 }
 
